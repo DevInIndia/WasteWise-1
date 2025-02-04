7cb6dd0bb04486e30cea0140b3fc64209df5a87c
Arslaan <mohdarslaanazam@gmail.com>
2025-02-04 22:16:56 +0530

    responsive about page updated

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,18 +7,12 @@ export default function Home() {
   const { data: session } = useSession();
 
   return (
-<<<<<<< HEAD
-    <main className="p-4">
-      <h1>WasteWise</h1>
-      
-=======
     <main>
       {session ? (
         <PromptPage/>
       ) : (
         <h1>WasteWise</h1>
       )}
->>>>>>> e1620bccd6b35fb656f4ee6e33f4c026c9ab37d0
     </main>
   );
 }
